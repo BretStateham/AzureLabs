4e9eae550d46253cda6e65fc34a87ada9d83bbc3
Bret Stateham <Bret.Stateham@microsoft.com>
2014-03-17 11:28:49 -0700

    Module 2

---
+++ Scenario/2-ServicesArchitecture/Source/End/Expenses WPF/Expenses.LabTests/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Expenses.Model;
+using Expenses.Model.Fakes;
+using Expenses.ViewModel;
+using Expenses.ViewModel.Fakes;
+using System.Threading.Tasks;
+
+
+namespace Expenses.LabTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public async Task TestMethod1()
+    {
+      ServiceLocator.Current.SetService<INavigationService>(
+        new StubINavigationService());
+
+      StubIViewService viewService =
+        new StubIViewService()
+        {
+          ExecuteBusyActionAsyncFuncOfTask =
+              async (Func<Task> func) =>
+              {
+                await func();
+              }
+        };
+      ServiceLocator.Current.SetService<IViewService>(viewService);
+
+      StubIExpenseRepository repository =
+        new StubIExpenseRepository()
+        {
+          GetChargeAsyncInt32 =
+              (chargeId) =>
+              {
+                return Task.FromResult(
+                    new Charge()
+                    {
+                      ChargeId = chargeId,
+                    });
+              }
+        };
+      ServiceLocator.Current.SetService<IExpenseRepository>(repository);
+
+
+
+
+      // Create a new ChargeViewModel.
+      ChargeViewModel chargeViewModel = new ChargeViewModel();
+
+      // Make sure it defaults to a ChargeId of 0.
+      Assert.AreEqual(0, chargeViewModel.ChargeId);
+
+      // Load the charge with the ChargeId of 1.
+      await chargeViewModel.LoadAsync(1);
+
+      // Confirm the ViewModel’s ChargeId is 1.
+      Assert.AreEqual(1, chargeViewModel.ChargeId);
+
+    }
+  }
+}
